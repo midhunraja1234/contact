15f61bdf8b5bc08c5675349c3629b3d4773292f8
midhunraja1234 <midhunraja1234@gmail.com>
2024-09-26 11:33:00 +0530

    testing

--- src/ContactList.jsx
+++ src/ContactList.jsx
@@ -29,7 +29,7 @@ function ContactList() {
                     }
                 </div>
                 <button className='add-contact' onClick={() => navigate('/add')}>
-                    <AiOutlinePlus className='add-icon' style={{ color: 'white' }} /> Add  new contact
+                    <AiOutlinePlus className='add-icon' style={{ color: 'white' }} /> Add Contacsss
                 </button>
             </div>
         </div>
