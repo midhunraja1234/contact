7f11f6ad2ff13f38108759534c9c33da5317006c
prasanth45bit <prasanthpalanisamy4@gmail.com>
2024-09-26 11:45:57 +0530

    By prasanth

--- src/ContactList.jsx
+++ src/ContactList.jsx
@@ -29,7 +29,7 @@ function ContactList() {
                     }
                 </div>
                 <button className='add-contact' onClick={() => navigate('/add')}>
-                    <AiOutlinePlus className='add-icon' style={{ color: 'white' }} /> Add contacts
+                    <AiOutlinePlus className='add-icon' style={{ color: 'white' }} />contacts
                 </button>
             </div>
         </div>
